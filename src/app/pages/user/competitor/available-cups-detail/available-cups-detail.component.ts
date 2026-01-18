d77e5defea6eb390fdd3c52a5a46b2b3b7398875
Rafael <rafaeltabolka@hotmail.com>
2026-01-18 15:16:43 -0300

    Criada tela de detalhes das copas disponiveis

--- src/app/pages/user/competitor/available-cups-detail/available-cups-detail.component.ts
+++ src/app/pages/user/competitor/available-cups-detail/available-cups-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-available-cups-detail',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './available-cups-detail.component.html',
   styleUrl: './available-cups-detail.component.css'
 })
