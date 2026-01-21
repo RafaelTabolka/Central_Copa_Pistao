07927299fd47c5f4fe885876569b87f938329711
Rafael <rafaeltabolka@hotmail.com>
2026-01-21 10:08:40 -0300

    Criada tela Copas do perfil admin

--- src/app/pages/user/admin/cups/cups.component.ts
+++ src/app/pages/user/admin/cups/cups.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cups',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cups.component.html',
   styleUrl: './cups.component.css'
 })
