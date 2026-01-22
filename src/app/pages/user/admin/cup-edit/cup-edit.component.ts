cc4c2e1ce924a9d9d25a61b3d19928b060212412
Rafael <rafaeltabolka@hotmail.com>
2026-01-21 22:24:33 -0300

    Criada tela para editar copas cadastradas

--- src/app/pages/user/admin/cup-edit/cup-edit.component.ts
+++ src/app/pages/user/admin/cup-edit/cup-edit.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-cup-edit',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cup-edit.component.html',
   styleUrl: './cup-edit.component.css'
 })
